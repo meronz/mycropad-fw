3a7ca1ab64f5474b947561fcba45e4ebb7a00614
Salvatore Merone <sal.merone95@gmail.com>
2021-12-20 19:35:29 +0100

    New board revision pinout

--- fw/include/consts.h
+++ fw/include/consts.h
@@ -6,17 +6,17 @@ extern "C"
 {
 #endif
 
-#define GPIO_SW_1 (1)
-#define GPIO_SW_2 (2)
-#define GPIO_SW_3 (3)
-#define GPIO_SW_4 (4)
-#define GPIO_SW_5 (5)
-#define GPIO_SW_6 (6)
-#define GPIO_SW_7 (7)
-#define GPIO_SW_8 (8)
-#define GPIO_ROT_CLK (9)
-#define GPIO_ROT_DATA (10)
-#define GPIO_ROT_SW (11)
+#define GPIO_SW_1 (27)
+#define GPIO_SW_2 (26)
+#define GPIO_SW_3 (22)
+#define GPIO_SW_4 (21)
+#define GPIO_SW_5 (17)
+#define GPIO_SW_6 (16)
+#define GPIO_SW_7 (14)
+#define GPIO_SW_8 (15)
+#define GPIO_ROT_CLK (19)
+#define GPIO_ROT_DATA (20)
+#define GPIO_ROT_SW (18)
 #define GPIO_LEDS (28)
 
 #define LEDS_RGBW (false)
