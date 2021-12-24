63d098d5543ec534264a54e0ffd6cf5f7c2646a2
Salvatore Merone <sal.merone95@gmail.com>
2021-12-24 13:45:57 +0100

    Leds brightness fix, added "off" pattern

--- fw/src/leds.cpp
+++ fw/src/leds.cpp
@@ -26,6 +26,15 @@ static inline uint32_t urgb_u32(uint8_t r, uint8_t g, uint8_t b)
 
 static volatile bool fixed_map_changed = true;
 static uint32_t fixed_map[LEDS_NUM];
+static const uint8_t max_brightness = 128; // max 255
+
+void pattern_off(uint len)
+{
+    for (uint i = 0; i < len; i++)
+    {
+        put_pixel(0);
+    }
+}
 
 void pattern_fixed(uint len)
 {
@@ -33,7 +42,13 @@ void pattern_fixed(uint len)
     {
         for (size_t i = 0; i < len; i++)
         {
-            put_pixel(fixed_map[i]);
+            uint8_t r = (fixed_map[i] >> 8) & 0xFF;
+            uint8_t g = (fixed_map[i] >> 16) & 0xFF;
+            uint8_t b = (fixed_map[i]) & 0xFF;
+            put_pixel(urgb_u32(
+                (int)((r * max_brightness) / 0xFF) & 0xFF,
+                (int)((g * max_brightness) / 0xFF) & 0xFF,
+                (int)((b * max_brightness) / 0xFF) & 0xFF));
         }
         fixed_map_changed = false;
     }
@@ -45,8 +60,7 @@ void pattern_rainbow(uint len)
     static float r = 0;
     static float g = 0;
     static float b = 0;
-    static const float speed = 1.0;
-    uint8_t maxBrightness = 128; // max 255
+    static const float speed = 2;
 
     switch(step)
     {
@@ -67,22 +81,22 @@ void pattern_rainbow(uint len)
     for (uint i = 0; i < len; i++)
     {
         put_pixel(urgb_u32(
-            (int)((r * maxBrightness) / 0xFF) & 0xFF,
-            (int)((g * maxBrightness) / 0xFF) & 0xFF,
-            (int)((b * maxBrightness) / 0xFF) & 0xFF));
+            (int)((r * max_brightness) / 0xFF) & 0xFF,
+            (int)((g * max_brightness) / 0xFF) & 0xFF,
+            (int)((b * max_brightness) / 0xFF) & 0xFF));
     }
 }
 
-#define PATTERNS_NUM (2)
+#define PATTERNS_NUM (3)
 typedef void (*pattern)(uint len);
-pattern patterns[PATTERNS_NUM] = { pattern_fixed, pattern_rainbow };
+pattern patterns[PATTERNS_NUM] = { pattern_off, pattern_fixed, pattern_rainbow };
 
 void Leds::Init()
 {
     _gpio = GPIO_LEDS;
     _isRGBW = LEDS_RGBW;
     _numLeds = LEDS_NUM;
-    _currentPattern = 1;
+    _currentPattern = 0;
 
     memset(fixed_map, 0, sizeof(fixed_map));
 
